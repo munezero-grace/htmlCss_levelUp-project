48ba7cbfc7a37cdc97c66349739bb38d8d983929
Grace <munezero.grace911@gmail.com>
2023-04-24 20:26:59 +0200

    javascript commit

---
+++ javascript/javascript.js
@@ -0,0 +1,33 @@
+//string concactination
+//variable declaration
+//let,const
+
+let FirstName= "Grace"
+let SecondName= "Munezero"
+let ThirdName="Axel"
+// console.log(FirstName + " " + SecondName)
+
+
+
+// console.log("welcome to my page");
+// console.log("2"+"2");
+// console.log(2+2);
+
+if(FirstName==="Grace" || SecondName==="Munezero"){
+    console.log("that's true");
+}
+else{
+    console.log("invalid FirstName")
+}
+
+
+// if(SecondName==="Munezero" &&FirstName==="Grace"){
+//     console.log("Admin")
+
+// }
+// else if(SecondName==="Munezero"&& ThirdName==="Axel"){
+// console.log("User")
+// }
+// else{
+//     console.log("Guest")
+// }
